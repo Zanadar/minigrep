4933edac19f3d1e4129383dac6f98c2aa4de0997
bot <witchcraft@example.com>
2018-09-25 18:05:10 -0400

    Start

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+use std::fs;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename).expect("Something went wrong with the file");
+    println!("Contents {}", contents);
 }
